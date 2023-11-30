bc51544ea314ae504f3d9bc1641cf8222a9a5b90
Awais Kamran <awaiskamran@Aribas-MacBook-Pro.local>
2023-11-30 11:27:58 +0500

    feat: interface files  updated

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/exploring-angular/browser'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/exploring-angular/browser'}),
 );
 
 // Start the app by listening on the default Heroku port
